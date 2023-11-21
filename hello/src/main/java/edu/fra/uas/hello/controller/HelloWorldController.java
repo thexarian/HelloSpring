e3dbfb5a0d56e6cea0a0c7b85bcd4c64e2f9e47e
thexarian <135020773+thexarian@users.noreply.github.com>
2023-11-21 16:44:09 +0000

    Added first Spring Boot Project

---
+++ hello/src/main/java/edu/fra/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package edu.fra.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller 
+public class HelloWorldController {
+    
+    @RequestMapping(value="path", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return  "<h1>Hello World Spring Boot!</h1>";
+    }
+    
+}
